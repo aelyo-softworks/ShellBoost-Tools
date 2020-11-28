f012aea6995167a9b0c0e3db228831a411e8f083
Aelyo Softworks <contact@aelyo.com>
2020-11-28 09:49:21 +0100

    Added DragDropViewer tool to help see what's being dragged (clipboard, IDataObject, etc.).

---
+++ DragDropViewer/DragDropViewer/Main.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace DragDropViewer
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.textBoxDragZone = new System.Windows.Forms.TextBox();
+            this.labelHint = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxDragZone
+            // 
+            this.textBoxDragZone.BackColor = System.Drawing.Color.White;
+            this.textBoxDragZone.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxDragZone.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxDragZone.Location = new System.Drawing.Point(0, 0);
+            this.textBoxDragZone.Multiline = true;
+            this.textBoxDragZone.Name = "textBoxDragZone";
+            this.textBoxDragZone.ReadOnly = true;
+            this.textBoxDragZone.Size = new System.Drawing.Size(984, 761);
+            this.textBoxDragZone.TabIndex = 0;
+            // 
+            // labelHint
+            // 
+            this.labelHint.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelHint.AutoSize = true;
+            this.labelHint.BackColor = System.Drawing.Color.White;
+            this.labelHint.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelHint.Location = new System.Drawing.Point(8, 8);
+            this.labelHint.Name = "labelHint";
+            this.labelHint.Size = new System.Drawing.Size(214, 30);
+            this.labelHint.TabIndex = 1;
+            this.labelHint.Text = "Drag Something Here";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 761);
+            this.Controls.Add(this.labelHint);
+            this.Controls.Add(this.textBoxDragZone);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Drag & Drop View";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxDragZone;
+        private System.Windows.Forms.Label labelHint;
+    }
+}
+
---
+++ DragDropViewer/DragDropViewer/Main.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using ShellBoost.Core;
+using ShellBoost.Core.WindowsShell;
+
+namespace DragDropViewer
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            AllowDrop = true;
+            labelHint.BackColor = textBoxDragZone.BackColor;
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            labelHint.Left = (Width - labelHint.Width) / 2;
+            labelHint.Top = (Height - labelHint.Height) / 2;
+            base.OnResize(e);
+        }
+
+        protected override void OnDragEnter(DragEventArgs drgevent)
+        {
+            labelHint.Visible = false;
+            drgevent.Effect = DragDropEffects.None;
+            var sb = new StringBuilder();
+            foreach (var format in drgevent.Data.GetFormats())
+            {
+                sb.AppendLine(format);
+
+                var data = drgevent.Data.GetData(format);
+                if (ShellDataObjectFormat.TryParseDataUsingKnownFormats(format, data, out var obj))
+                {
+                    if (obj is IReadOnlyList<ShellItemIdList> idls)
+                    {
+                        for (var i = 0; i < idls.Count; i++)
+                        {
+                            var idl = idls[i];
+                            sb.AppendLine(" idl#" + i + ": " + idl.GetPath());
+                        }
+                    }
+                    else if (obj is IReadOnlyList<FILEDESCRIPTOR> descs)
+                    {
+                        for (var i = 0; i < descs.Count; i++)
+                        {
+                            var desc = descs[i];
+                            sb.AppendLine(" desc#" + i + ": " + desc);
+                        }
+                    }
+                    else
+                    {
+                        sb.AppendLine(" " + obj + " (" + (obj?.GetType().Name) + ")");
+                    }
+                }
+                else
+                {
+                    if (data is MemoryStream ms && ms.Length == 4)
+                    {
+                        var value = BitConverter.ToInt32(ms.ToArray(), 0);
+                        sb.AppendLine(" " + GetDataString(data) + " (" + (data?.GetType().Name) + "): " + value + " 0x" + value.ToString("X8"));
+                    }
+                    else
+                    {
+                        sb.AppendLine(" " + GetDataString(data) + " (" + (data?.GetType().Name) + ")");
+                    }
+                }
+                sb.AppendLine();
+            }
+            textBoxDragZone.Text = sb.ToString();
+        }
+
+        private static string GetDataString(object data)
+        {
+            if (data == null)
+                return "<null>";
+
+            if (data is string[] strings)
+                return string.Join("| ", strings);
+
+            if (data is MemoryStream ms)
+                return "len:" + ms.Length;
+
+            return string.Format("{0}", data);
+        }
+    }
+}
