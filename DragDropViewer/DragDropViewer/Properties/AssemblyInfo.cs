a42620905ce8bbc4f760e0780af263111da35991
Aelyo Softworks <contact@aelyo.com>
2021-01-02 09:51:34 +0100

    Updated year.

---
+++ DragDropViewer/DragDropViewer/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+#if DEBUG
+[assembly: AssemblyTitle("DragDropViewer - Debug")]
+[assembly: AssemblyConfiguration("DEBUG")]
+#else
+[assembly: AssemblyTitle("DragDropViewer - Release")]
+[assembly: AssemblyConfiguration("RELEASE")]
+#endif
+[assembly: AssemblyDescription("ShellBoost Tools - Drag & Drop Viewer")]
+[assembly: AssemblyCompany("Aelyo Softworks")]
+[assembly: AssemblyProduct("Shell Namespace Extension Booster")]
+[assembly: AssemblyCopyright("Copyright (C) 2017-2021 Aelyo Softworks. All rights reserved.")]
+[assembly: AssemblyTrademark("ShellBoost (TM) is a trademark of Aelyo Softworks.")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5f7f7b90-d0c3-410d-8acb-26d563100ea0")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
